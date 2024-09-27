6039aecbdd70a9c6787af7f53c1f4212ddd5d123
PedroSmaxY <pedrohnovais@outlook.com>
2024-09-27 18:44:18 -0300

    refactor: change code exceptions treatment for better documentation

--- src/main/java/com/pooproject/products_system/dao/ProductDAO.java
+++ src/main/java/com/pooproject/products_system/dao/ProductDAO.java
@@ -1,58 +1,63 @@
 package com.pooproject.products_system.dao;
 
+import com.pooproject.products_system.exceptions.ProductDAOException;
 import com.pooproject.products_system.models.Product;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
-import jakarta.transaction.Transactional;
 
 import java.util.List;
 
 public class ProductDAO {
 
-    private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
 
-    @Transactional
     public void save(Product product) {
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        em.persist(product);
-        em.getTransaction().commit();
-        em.close();
+        try (EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            em.persist(product);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            throw new ProductDAOException("Failed to save product", e);
+        }
     }
 
     public Product findById(Long id) {
-        EntityManager em = emf.createEntityManager();
-        Product product = em.find(Product.class, id);
-        em.close();
-        return product;
-    }
-
-    @Transactional
-    public void update(Product product) {
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        em.merge(product);
-        em.getTransaction().commit();
-        em.close();
-    }
-
-    @Transactional
-    public void delete(Long id) {
-        EntityManager em = emf.createEntityManager();
-        Product product = em.find(Product.class, id);
-        if (product != null) {
-            em.getTransaction().begin();
-            em.remove(product);
-            em.getTransaction().commit();
+        try (EntityManager em = emf.createEntityManager()) {
+            return em.find(Product.class, id);
+        } catch (Exception e) {
+            throw new ProductDAOException("Failed to find product with id: " + id, e);
         }
-        em.close();
+    }
+
+    public void update(Product product) {
+        try (EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            em.merge(product);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            throw new ProductDAOException("Failed to update product", e);
+        }
+    }
+
+    public void delete(Long id) {
+        try (EntityManager em = emf.createEntityManager()) {
+            Product product = em.find(Product.class, id);
+            if (product != null) {
+                em.getTransaction().begin();
+                em.remove(product);
+                em.getTransaction().commit();
+            }
+        } catch (Exception e) {
+            throw new ProductDAOException("Failed to delete product with id: " + id, e);
+        }
     }
 
     public List<Product> findAll() {
-        EntityManager em = emf.createEntityManager();
-        List<Product> products = em.createQuery("from Product", Product.class).getResultList();
-        em.close();
-        return products;
+        try (EntityManager em = emf.createEntityManager()) {
+            return em.createQuery("from Product", Product.class).getResultList();
+        } catch (Exception e) {
+            throw new ProductDAOException("Failed to retrieve product list", e);
+        }
     }
 }
