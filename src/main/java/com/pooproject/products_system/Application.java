df50e761fae68e812083b47cc87acbee585a8207
PedroSmaxY <pedrohnovais@outlook.com>
2024-09-27 18:02:34 -0300

    test: temporary db test added

--- src/main/java/com/pooproject/products_system/Application.java
+++ src/main/java/com/pooproject/products_system/Application.java
@@ -1,7 +1,11 @@
 package com.pooproject.products_system;
 
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("olá mundo!");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
     }
 }
