eede0207cce8aa4d8e91badead0f8d07890572a1
PedroSmaxY <pedrohnovais@outlook.com>
2024-09-27 18:03:04 -0300

    feat: product entity added properly

--- src/main/java/com/pooproject/products_system/models/Product.java
+++ src/main/java/com/pooproject/products_system/models/Product.java
@@ -1,4 +1,31 @@
 package com.pooproject.products_system.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Table(name = Product.TABLE_NAME)
 public class Product {
+    protected static final String TABLE_NAME = "products";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private Integer quantity;
 }
