939a8e8318d064fb06e803406a9a7901b8015522
PedroSmaxY <pedrohnovais@outlook.com>
2024-11-04 17:34:46 -0300

    fix: removed old view and added new one

--- src/main/java/com/pooproject/products_system/Main.java
+++ src/main/java/com/pooproject/products_system/Main.java
@@ -1,11 +1,12 @@
 package com.pooproject.products_system;
 
 import com.pooproject.products_system.dao.ProductDAO;
+import com.pooproject.products_system.views.MainView;
 
 
 public class Main {
     public static void main(String[] args) {
         var dao = new ProductDAO();
-        //new TelaMenu().setVisible(true);
+        new MainView().setVisible(true);
     }
 }
