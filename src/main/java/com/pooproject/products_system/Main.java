3c70cdec6f2a9932752b5766de2233587e1960e8
PedroSmaxY <pedrohnovais@outlook.com>
2024-11-24 17:29:10 -0300

    feat: preparing program to deploy

--- src/main/java/com/pooproject/products_system/Main.java
+++ src/main/java/com/pooproject/products_system/Main.java
@@ -1,12 +1,12 @@
 package com.pooproject.products_system;
 
-import com.pooproject.products_system.dao.ProductDAO;
 import com.pooproject.products_system.views.MainView;
 
 
 public class Main {
     public static void main(String[] args) {
-        var dao = new ProductDAO();
-        new MainView().setVisible(true);
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            new MainView().setVisible(true);
+        });
     }
 }
